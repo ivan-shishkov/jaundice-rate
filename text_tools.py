5f98cc5bc0db37bc15ae80d6cc1a358ffadfb0b3
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-07-17 10:07:15 +0300

    Deleted redundant comments

--- text_tools.py
+++ text_tools.py
@@ -7,7 +7,6 @@ import pytest
 
 def _clean_word(word):
     word = word.replace('«', '').replace('»', '').replace('…', '')
-    # FIXME какие еще знаки пунктуации часто встречаются ?
     word = word.strip(string.punctuation)
     return word
 
@@ -26,8 +25,6 @@ async def split_by_words(morph, text):
 
 @pytest.mark.asyncio
 async def test_split_by_words():
-    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
-    # Старайтесь ораганизовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
     morph = pymorphy2.MorphAnalyzer()
 
     assert await split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
